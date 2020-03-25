29fef762a7ad7956d4d64a439b4a0e8fb3d36d1d
Nick Schmidt <ionic.is.cool@gmail.com>
2020-03-25 13:49:01 +0100

    Coins met UI

---
+++ Assets/Scripts/Environment/playerData.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playerData : MonoBehaviour
+{
+    public float health = 100f;
+
+    public Text coinTxt;
+    public float coins;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "health")
+        {
+            health = 125f;
+            Destroy(collision.gameObject);
+        }
+
+        else if (collision.tag == "coin")
+        {
+            coins += 1f;
+            coinTxt.text = coins.ToString();
+            Destroy(collision.gameObject);
+        }
+    }
+    void Start()
+    {
+        coinTxt.text = coins.ToString();
+    }
+}
