4a8766e0e0e0406e1ed30b2e59ab02399fc84669
Paasei <45030613+Isisisdoodvanbinnen@users.noreply.github.com>
2020-04-01 14:05:25 +0200

    no message

---
+++ Assets/Scripts/test/doubleJump.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doubleJump : MonoBehaviour
+{
+
+    public float speed;
+    public float jumpForce;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+    public int extraJumpValue;
+   
+    private float moveInput;
+    private Rigidbody2D rb;
+    private bool facingRight = true;
+    private bool isGrounded;
+    private int extraJumps;
+
+
+
+
+    void Start()
+    {
+        extraJumps = extraJumpValue;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+           if (isGrounded == true)
+        {
+            extraJumps = extraJumpValue;
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && extraJumps > 0)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+            extraJumps--;
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow) && jumpForce == 0 && isGrounded == true)
+        {
+
+        }
+    }
+
+    void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+        if (facingRight == false && moveInput > 0)
+        {
+            Flip();
+        }
+        else if (facingRight == true && moveInput < 0)
+        {
+            Flip();
+        }
+
+
+        void Flip()
+        {
+            facingRight = !facingRight;
+            Vector3 Scaler = transform.localScale;
+            Scaler.x *= -1;
+            transform.localScale = Scaler;
+        }
+    }
+}
