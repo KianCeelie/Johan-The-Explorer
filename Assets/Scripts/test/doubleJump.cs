7134ce97713558b5511bf3ee7bb739738665737d
Paasei <45030613+Isisisdoodvanbinnen@users.noreply.github.com>
2020-04-08 14:04:34 +0200

    no message

--- Assets/Scripts/test/doubleJump.cs
+++ Assets/Scripts/test/doubleJump.cs
@@ -17,9 +17,7 @@ public class doubleJump : MonoBehaviour
     private bool facingRight = true;
     private bool isGrounded;
     private int extraJumps;
-
-
-
+    private bool dubbleJumpIsActive = false;
 
     void Start()
     {
@@ -29,29 +27,50 @@ public class doubleJump : MonoBehaviour
 
     void Update()
     {
-           if (isGrounded == true)
+        if (dubbleJumpIsActive == true)
         {
-            extraJumps = extraJumpValue;
-        }
+            if (isGrounded == true)
+            {
+                extraJumps = extraJumpValue;
+            }
 
-        if (Input.GetKeyDown(KeyCode.UpArrow) && extraJumps > 0)
-        {
-            rb.velocity = Vector2.up * jumpForce;
-            extraJumps--;
+            if (Input.GetKeyDown(KeyCode.UpArrow) && extraJumps > 0)
+            {
+                rb.velocity = Vector2.up * jumpForce;
+                extraJumps--;
+            }
+            else if (Input.GetKeyDown(KeyCode.UpArrow) && jumpForce == 0 && isGrounded == true)
+            {
+                rb.velocity = Vector2.up * jumpForce;
+            }
         }
-        else if (Input.GetKeyDown(KeyCode.UpArrow) && jumpForce == 0 && isGrounded == true)
+        else
         {
-
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                rb.velocity = Vector2.up * jumpForce;
+                extraJumps--;
+            }
         }
     }
 
-    void FixedUpdate()
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "jump")
+        {
+            Destroy(collision.gameObject);
+            dubbleJumpIsActive = true;
+        }
+
+    }
+
+    private void FixedUpdate()
     {
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
 
+
         moveInput = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
-
         if (facingRight == false && moveInput > 0)
         {
             Flip();
@@ -60,9 +79,10 @@ public class doubleJump : MonoBehaviour
         {
             Flip();
         }
+    }
 
 
-        void Flip()
+    void Flip()
         {
             facingRight = !facingRight;
             Vector3 Scaler = transform.localScale;
@@ -70,4 +90,4 @@ public class doubleJump : MonoBehaviour
             transform.localScale = Scaler;
         }
     }
-}
+
