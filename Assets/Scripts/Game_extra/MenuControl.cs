31e504c95a67044449876febbad11f2c8f5dc238
Paasei <45030613+Isisisdoodvanbinnen@users.noreply.github.com>
2020-04-23 09:34:14 +0200

    Startscherm

---
+++ Assets/Scripts/Game_extra/MenuControl.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuControl : MonoBehaviour
+{
+
+    public GameObject OptionsPanel;
+    public GameObject Neusaap;
+
+    public void LoadScene(string level)
+    {
+        SceneManager.LoadScene(level);
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Startscherm");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void LoadOptionsPanel()
+    {
+        OptionsPanel.gameObject.SetActive(true);
+    }
+
+    public void CloseOptionsPanel()
+    {
+        OptionsPanel.gameObject.SetActive(false);
+    }
+
+    public void LoadNeusaap()
+    {
+        Neusaap.gameObject.SetActive(true);
+    }
+
+    public void CloseNeusaap()
+    {
+        Neusaap.gameObject.SetActive(false);
+    }
+}
