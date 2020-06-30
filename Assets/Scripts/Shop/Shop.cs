f6c3805f5c93124f23b3c1026910f89cb55a9dfd
Nick Schmidt <ionic.is.cool@gmail.com>
2020-06-30 14:18:16 +0200

    Shop werkt

--- Assets/Scripts/Shop/Shop.cs
+++ Assets/Scripts/Shop/Shop.cs
@@ -7,15 +7,39 @@ public class Shop : MonoBehaviour
     public string itemName;
     public float itemCost;
     public string itemDesc;
- 
-    // Start is called before the first frame update
-    void Start()
-    {
+    public float inStock;
+    public float i = 0;
 
+    public CoinsScript coins; 
+
+    public void Health()
+    {
+        itemName = "Levens";
+        itemCost = 10;
+        inStock = 1;
+        Buy();
     }
 
-    void Update()
+    public void Buy()
     {
-        
+        if (i < inStock && coins.coinsAantal == itemCost)
+        {
+            Debug.Log("Bedankt voor het kopen van " + itemName + ".");
+            coins.coinsAantal -= itemCost;
+            coins.coinTxt.text = coins.coinsAantal.ToString();
+            i++;
+        }
+        else if (i < inStock && coins.coinsAantal != itemCost)
+        {
+            Debug.Log("Je hebt te weinig geld voor " + itemName + "!");
+        }
+        else if (i >= inStock && coins.coinsAantal == itemCost)
+        {
+            Debug.Log(itemName + " is uitverkocht");
+        }
+        else if (i >= inStock && coins.coinsAantal != itemCost)
+        {
+            Debug.Log(itemName + " is uitverkocht");
+        }
     }
 }
